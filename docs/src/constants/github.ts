dfe6bc60f84c2a85a590f6716c38654185dfc390
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-02 17:27:27 -0500

    Add constants for GitHub links and project version

---
+++ docs/src/constants/github.ts
@@ -0,0 +1,10 @@
+const OWNER_PROFILE_URL = "https://github.com/Diego17cp"
+const REPO_URL = `${OWNER_PROFILE_URL}/dialcash`
+
+export const GITHUB_LINKS = {
+    OWNER: OWNER_PROFILE_URL,
+    REPO: REPO_URL,
+    ISSUES: `${REPO_URL}/issues`,
+    PULL_REQUESTS: `${REPO_URL}/pulls`,
+    RELEASES: `${REPO_URL}/releases`,
+}
---
+++ docs/src/constants/project.ts
@@ -0,0 +1,3 @@
+export const PROJECT = {
+    VERSION: "1.0.0",
+}
