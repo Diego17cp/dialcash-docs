e8585b14a86253454eb5afaabb5a64d156553d0d
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-02 17:15:15 -0500

    Add WhatIsNot component to clarify Dialcash's functionality and privacy features

---
+++ docs/src/features/guide/index.ts
@@ -0,0 +1,5 @@
+export { default as GuideHero } from './GuideHero.astro';
+export { default as GuideFlow } from './GuideFlow.astro';
+export { default as GuideContent } from './GuideContent.astro';
+export { default as BestPractices } from "./BestPractices.astro";
+export { default as WhatIsNot } from "./WhatIsNot.astro";
