e5c897080a4dca6536b4401026df8b3847c18b10
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-02 23:55:27 -0500

    Add HowToInstall component with installation steps for Dialcash

---
+++ docs/src/features/download/index.ts
@@ -0,0 +1,3 @@
+export { default as DownloadHero } from './DownloadHero.astro';
+export { default as InfoSection } from './InfoSection.astro';
+export { default as HowToInstall } from './HowToInstall.astro';
