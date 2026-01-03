7ebfe8a563503ed069623e5f98825e00dd775e48
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-03 12:13:41 -0500

    Add ProjectStatus component to display project transparency and updates

---
+++ docs/src/features/support/index.ts
@@ -0,0 +1,5 @@
+export { default as SupportHero } from './SupportHero.astro';
+export { default as QuickHelpOptions } from './QuickHelpOptions.astro';
+export { default as CommonIssues } from './CommonIssues.astro';
+export { default as ContactSection } from './ContactSection.astro';
+export { default as ProjectStatus } from './ProjectStatus.astro';
